5bd4fe101db18c9dc1615623a63da44adc89773c
Somnath Dey <sdey.aklakhi@gmail.com>
2023-05-20 12:56:11 +0530

    end session 12

--- Session10/models/File.js
+++ Session10/models/File.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -14,6 +16,34 @@ const fileSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+//post middleware
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC", doc);
+
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    //send mail
+    let info = await transporter.sendMail({
+      from: "Codophile -by Somnath",
+      to: doc.email,
+      subject: "New file uploaded on Cloudinary",
+      html: `<h2>Hello Jee</h2> <p>File Uploaded View Here: <a href="${doc.imageUrl}">Link</a> </p>`,
+    });
+
+    console.log("Info", info);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const File = mongoose.model("File", fileSchema);
